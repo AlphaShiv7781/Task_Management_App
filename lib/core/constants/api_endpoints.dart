a03f865e73413b3fe3950ca4b8974dbceb3fbf24
Shivam Sharma <shivamsharma242424@gmail.com>
2026-01-21 18:45:46 +0530

    URL changed

--- lib/core/constants/api_endpoints.dart
+++ lib/core/constants/api_endpoints.dart
@@ -1,5 +1,5 @@
 class ApiEndpoints {
-  static const baseUrl = "https://task-manager-backend-7j1a.onrender.com";
+  static const baseUrl = "https://task-management-hwld.onrender.com/";
 
   static const login = "/auth/login";
   static const register = "/auth/register";
